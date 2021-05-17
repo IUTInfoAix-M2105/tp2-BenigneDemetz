72217a8c3d6341c7047980b9a74218fa8175c97a
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-05-17 09:22:48 +0200

    hello button set title

--- src/main/java/fr/univ_amu/iut/exercice7/HelloButton.java
+++ src/main/java/fr/univ_amu/iut/exercice7/HelloButton.java
@@ -1,11 +1,13 @@
 package fr.univ_amu.iut.exercice7;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class HelloButton extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("Hello !");
     }
 }

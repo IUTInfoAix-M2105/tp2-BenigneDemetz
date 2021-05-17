d428a6abac13acb0a816cc3b5ca4acff8cb7f562
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-05-17 11:06:50 +0200

    beautiful useful button avec nombre d'action fonctionnel

--- src/main/java/fr/univ_amu/iut/exercice10/HelloBeautifulUsefulButton.java
+++ src/main/java/fr/univ_amu/iut/exercice10/HelloBeautifulUsefulButton.java
@@ -1,13 +1,46 @@
 package fr.univ_amu.iut.exercice10;
 
+import fr.univ_amu.iut.exercice9.EcouteurSimple;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class HelloBeautifulUsefulButton extends Application {
     private int nbAction = 0;
 
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello !");
+        primaryStage.setHeight(250);
+        primaryStage.setWidth(250);
 
+        Button button = new Button("Hello !");
+        button.setId("buttonHello");
+
+        Image image = new Image("https://raw.githubusercontent.com/IUTInfoAix-M2105/Syllabus/master/assets/logo.png");
+        ImageView iv1 = new ImageView();
+        iv1.setImage(image);
+        button.setGraphic(iv1);
+
+        EcouteurSimple es = new EcouteurSimple();
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                nbAction += 1;
+                System.out.println("Bouton actionné " + String.valueOf(nbAction) + " fois");
+            }
+        });
+        Scene scene = new Scene(button, 250, 250);
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("DarkTheme.css").toExternalForm());
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
 }
+

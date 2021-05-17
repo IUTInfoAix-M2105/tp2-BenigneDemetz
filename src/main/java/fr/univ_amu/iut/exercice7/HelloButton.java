21bd8518ecdcbc5f2b9e919d0fc66f50e0ff37d3
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-05-17 09:23:07 +0200

    hello button isShowing

--- src/main/java/fr/univ_amu/iut/exercice7/HelloButton.java
+++ src/main/java/fr/univ_amu/iut/exercice7/HelloButton.java
@@ -9,5 +9,6 @@ public class HelloButton extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Hello !");
+        primaryStage.show();
     }
 }
